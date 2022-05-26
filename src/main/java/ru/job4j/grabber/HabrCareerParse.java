26782f774e68645c09d6e4c41111bdd666458b23
yungdanie <nikoches253@gmail.com>
2022-05-26 14:43:32 +0300

    2.1.1. Парсинг https://www.sql.ru/forum/job-offers/3 [#285210]

--- src/main/java/ru/job4j/grabber/HabrCareerParse.java
+++ src/main/java/ru/job4j/grabber/HabrCareerParse.java
@@ -18,23 +18,27 @@ public class HabrCareerParse {
 
     private static final String PAGE_LINK = String.format("%s/vacancies/java_developer", SOURCE_LINK);
 
+    private static final String PAGE_NUMBER = "?page=";
+
     public static void main(String[] args) throws IOException {
         HabrCareerDateTimeParser parser = new HabrCareerDateTimeParser();
-        Connection connection = Jsoup.connect(PAGE_LINK);
-        Document document = connection.get();
         List<Vacancy> vacancyList = new ArrayList<>();
-        Elements rows = document.select(".vacancy-card__inner");
-        rows.forEach(row -> {
-            Element dateElement = row.selectFirst(".vacancy-card__date").child(0);
-            String data = dateElement.attr("datetime");
-            Element titleElement = row.selectFirst(".vacancy-card__title");
-            Element linkElement = titleElement.child(0);
-            String vacancyName = titleElement.text();
-            vacancyList.add(new Vacancy
-                    (vacancyName,
-                            String.format("%s%s", SOURCE_LINK, linkElement.attr("href")),
-                            parser.parse(data)));
-        });
+        for (int i = 1; i <= 5; i++) {
+            Connection connection = Jsoup.connect(String.format("%s%s%d", PAGE_LINK, PAGE_NUMBER, i));
+            Document document = connection.get();
+            Elements rows = document.select(".vacancy-card__inner");
+            rows.forEach(row -> {
+                Element dateElement = row.selectFirst(".vacancy-card__date").child(0);
+                String data = dateElement.attr("datetime");
+                Element titleElement = row.selectFirst(".vacancy-card__title");
+                Element linkElement = titleElement.child(0);
+                String vacancyName = titleElement.text();
+                vacancyList.add(new Vacancy
+                        (vacancyName,
+                                String.format("%s%s", SOURCE_LINK, linkElement.attr("href")),
+                                parser.parse(data)));
+            });
+        }
         vacancyList.forEach(System.out::println);
     }
 
