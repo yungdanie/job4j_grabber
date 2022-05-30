fd7c38a02bc18d01f7d7d71474141991e1d2f0bb
yungdanie <nikoches253@gmail.com>
2022-05-31 02:09:23 +0300

    7. Web. [#289478 #307064]

--- src/main/java/ru/job4j/quartz/test/Test.java
+++ src/main/java/ru/job4j/quartz/test/Test.java
@@ -3,7 +3,7 @@ package ru.job4j.quartz.test;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
-public class test {
+public class Test {
     public static void main(String[] args) {
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
         System.out.println(LocalDateTime.now().format(formatter));
