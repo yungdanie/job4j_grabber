9868b23a6126c442ffbd18554c617f2b75d39fae
yungdanie <nikoches253@gmail.com>
2022-05-30 15:53:37 +0300

    4. Схема таблицы Post [#1731]

--- create_table.sql
+++ create_table.sql
@@ -1,5 +1,5 @@
 create table post(id serial primary key,
 			name text,
 			text text,
-			link text primary key,
-			created timestamp primary key);
+			link text unique,
+			created timestamp unique);
